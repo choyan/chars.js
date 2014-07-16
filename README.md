a2fa8ac92f67f38ef230c598cfd7f89a40d6e09f
Zahidul Hossain <choyan.009@gmail.com>
2014-07-17 02:47:16 +0600

    0.2: Now you dont need to specifically use id for textarea and class for text container. It is now more handy. Just provide the textarea position on first argument and text container id  or class name on the second argument

--- README.md
+++ README.md
@@ -19,6 +19,14 @@ In HTML:
 
 In js:
 
-`chars(400, "text-4");`
+`chars(400, "#text-1", ".text-1");`
 
-Here 400  is the maximum number of characters and "text-4" is the id and class name of the containers
+Here 400  is the maximum number of characters and "#text-1" is the id of textarea and ".text-1" class name of the text container
+
+
+Update:
+==========
+0.2:
+	Now you dont need to specifically use id for textarea and class for text container.
+	It is now more handy. Just provide the textarea position on first argument and text
+	container id  or class name on the second argument
--- chars.js
+++ chars.js
@@ -2,19 +2,19 @@
  * charsjs by Zahidul Hossain
  * Licensed under MIT License
  * Release Date: 8th July, 2014
- * Version 0.1
+ * Version 0.2
  */
 
-var max, position;
+var max, area, position;
 "use strict";
-function chars(max, position) {
-    $("#" + position).keyup(function() {
+function chars(max, area, position) {
+    $(area).keyup(function() {
         var len = $(this).val().length;
         var left = max - len;
         if (len >= max) {
-            $("." + position).text("You have reached the limit");
+            $(position).text("You have reached the limit");
         } else {
-            $("." + position).text(left + " characters left");
+            $(position).text(left + " characters left");
         }
     });
 }
